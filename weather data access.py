dbf3c884bb0acbd2019cc81c43bb5080cf0b4877
praveenr2998 <51623786+praveenr2998@users.noreply.github.com>
2019-07-26 23:45:57 +0530

    Update weather data access.py

--- weather data access.py
+++ weather data access.py
@@ -1,9 +1,11 @@
+# Weather Data Access From OPEN WEATHER API 
+
 import requests
 import json
  
 from pprint import pprint
  
-API_key = "b6264876af30a491aa320e16bf3f296b"
+API_key = "Enter the generated API key here "
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
  
 city_name = input("Enter a city Name : ")
